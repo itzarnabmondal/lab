b9e2ede228e3dccd010a9eb43453990f8b673d19
Arnab Mondal <110527370+arxmondal@users.noreply.github.com>
2022-08-12 18:00:10 +0530

    Update SwappingTwoVariables.java

--- 12thAugLab/SwappingTwoVariables.java
+++ 12thAugLab/SwappingTwoVariables.java
@@ -1,3 +1,5 @@
+// Q2. Swapping two variables
+
 public class SwappingTwoVariables {
     public static void main(String arg[]) {
         int x = 10;  
@@ -5,7 +7,8 @@ public class SwappingTwoVariables {
         System.out.println(":: Before swapping ::\n");    
         System.out.println("value of x:" + x);  
         System.out.println("value of y:" + y);  
-        System.out.println("\n:: After swapping ::\n");  
+        System.out.println("\n:: After swapping ::\n");
+        // Swapping
         x = x + y;  
         y = x - y;  
         x = x - y;  

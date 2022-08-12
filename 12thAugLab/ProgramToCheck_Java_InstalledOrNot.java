06096005d79470b75de9c35a60fd7bed50d5307e
Arnab Mondal <110527370+arxmondal@users.noreply.github.com>
2022-08-12 18:02:24 +0530

    Update ProgramToCheck_Java_InstalledOrNot.java

--- 12thAugLab/ProgramToCheck_Java_InstalledOrNot.java
+++ 12thAugLab/ProgramToCheck_Java_InstalledOrNot.java
@@ -1,5 +1,9 @@
+// Q3. Write a Java program to check whether Java is installed on your computer.
+
 public class ProgramToCheck_Java_InstalledOrNot {
     public static void main(String[] args) {
+        /* The getProperty method in Java is used to returns the 
+        system property denoted by the specified key passed as its argument. */
         System.out.println("\nJava Version: " + System.getProperty("java.version"));
         System.out.println("Java Runtime Version: " + System.getProperty("java.runtime.version"));
         System.out.println("Java Home: " + System.getProperty("java.home"));

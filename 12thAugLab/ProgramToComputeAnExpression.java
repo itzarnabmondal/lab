c30e241e0e9fdbf9ef64764d0480f07ba35999f5
Arnab Mondal <110527370+arxmondal@users.noreply.github.com>
2022-08-12 18:03:38 +0530

    Update ProgramToComputeAnExpression.java

--- 12thAugLab/ProgramToComputeAnExpression.java
+++ 12thAugLab/ProgramToComputeAnExpression.java
@@ -1,3 +1,6 @@
+/* Q4. Write a Java program to compute the specified expressions and print the output.
+Specified Expression: (25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5) */
+
 public class ProgramToComputeAnExpression {
 	public static void main(String[] args) {
 		float a=25.5F, b=3.5F, d=40.5F, e=4.5F;

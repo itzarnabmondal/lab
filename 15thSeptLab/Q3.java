d37bd228c010669a310cff7ec1f454de7e2f12cc
Arnab Mondal <arxxxmondal@gmail.com>
2022-09-16 14:26:56 +0530

    Add files via upload

---
+++ 15thSeptLab/Q3.java
@@ -0,0 +1,19 @@
+package com.question3;
+
+class A {
+
+}
+class B extends A {
+
+}
+class C extends B {
+
+}
+
+/**
+ * ANS: It's a Multilevel inheritance.
+ * When a class extends a class, which extends anther class then this is called
+ * multilevel inheritance. Class C extends class B and class B
+ * extends class A then this type of inheritance is known as multilevel
+ * inheritance.
+ */

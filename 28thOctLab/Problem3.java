5e8fd2878edf1ecd8d98ee8e2c2a0f40acd4ddcb
Arnab Mondal <arxmondal@hotmail.com>
2022-10-28 17:35:34 +0530

    Update Problem3.java

--- 28thOctLab/Problem3.java
+++ 28thOctLab/Problem3.java
@@ -1,3 +1,12 @@
+/**
+ * Q3.
+ * Write a method called addTwoPositive that takes two int 
+ * parameters, and if either value is not positive, throw an 
+ * ArithmeticException, passing the string "Non-positive 
+ * integer sent". to the constructor of the exception.if the
+ * values are both positive, then return the sum of them. 
+ */
+
 public class Problem3 {
 
     public static void main(String[] args) { // Main method
